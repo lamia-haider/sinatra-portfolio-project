907ae642e1d7666696fdb196e61f83fc89b82538
Lamia Haider <lamia.haider@gmail.com>
2021-03-11 01:35:24 -0500

    formatted sessions/show, added post /method to sessionscontroller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,16 @@ class SessionController < ApplicationController
         erb :'/sessions/new'
     end
 
+    post '/sessions' do
+        @new_session = Session.create(duration_minutes: params[:duration_minutes], mood_rating: params[:mood_rating])
+        @new_session.user = User.find_by(id: session[:user_id])
+        @new_session.save
+        redirect "/sessions/#{@new_session.id}"
+    end
+
     get '/sessions/:id' do
         @session = Session.find_by(params[:id])
         erb :'/sessions/show'
-
     end
+
 end

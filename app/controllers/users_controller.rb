60340ae0fb4307c3a0b13827e3a5cb7716041896
Lamia Haider <lamia.haider@gmail.com>
2021-03-10 22:37:58 -0500

    added basic user controller and login view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ class UsersController < ApplicationController
         if @user.username.empty? || @user.email.empty? || !@user.save
             flash[:notice] = "User creation unsuccessful, please try again."
             redirect '/signup'
-        else user.save
-            session[:user_id] = user.id
+        else @user.save
+            session[:user_id] = @user.id
+            flash[:notice] = "yay"
         end
     
         
@@ -24,7 +25,16 @@ class UsersController < ApplicationController
 
 
     get '/login' do
-        
+        erb :'/users/login'
+    end
+
+    post '/login' do
+        user= User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Welcome"
+        end
+
     end
 
 

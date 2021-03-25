96b5a4e360d795b48680296ced97e3ef0df15e7e
Lamia Haider <lamia.haider@gmail.com>
2021-03-25 09:01:48 -0400

    added more user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :sessions
     validates :username, presence: true
-    validates :username, length: { minimum: 1}
+    validates :username, uniqueness: true
+    validates :username, length: { minimum: 2}
     validates :email, presence: true
     validates :email, length: { minimum: 4}
+    validates :email, uniqueness: true
     validates :password, length: {minimum: 4}
 
 end
